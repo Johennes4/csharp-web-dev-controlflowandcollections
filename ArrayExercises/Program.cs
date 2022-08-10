60702bc6940a3abaf3aec64c30d478814326da80
Zoey Martin <tunah.fr@gmail.com>
2022-08-10 17:19:08 -0500

    chapter3 exercises

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,28 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+
+            int[] numArray = { 1, 1, 2, 3, 5, 8 };
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                Console.WriteLine(numArray[i]);
+
+
+            }
+
+            foreach (int num in numArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                if (numArray[i] % 2 != 0)
+                {
+                    Console.WriteLine(numArray[i]);
+                }
+            };
         }
     }
 }
--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -7,6 +7,16 @@ namespace StringExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Strings!");
+
+            string sentence = ("I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.");
+
+            string[] words = sentence.Split(' ');
+
+            Console.WriteLine(string.Join(" ", words));
+
+            string[] seperateSentences = sentence.Split(".");
+
+            Console.WriteLine(string.Join(".", seperateSentences));
         }
     }
 }
